19afba727c0ab3ad11953cfdcd3b608fdd388eda
ajpx01 <ajpoon321@gmail.com>
2026-02-18 16:34:40 -0500

    Edits to Exercise 3

--- runhouseholder_reflect.m
+++ runhouseholder_reflect.m
@@ -5,11 +5,13 @@ I=eye(size(U)); % Set the identity matrix equal to the size of U
 Q=house_apply(U,I) % Solve for value Q from U and I
 QR=Q*R % Show that QR=Q*R and that the decomposition worked
 
-A1=rand(100,100)
-[Q1 R1]=gs_factor(A1);
-Step1=Q1'*Q1 % Equals I the identity matrix
-Step2=Q1*Q1' % Equals I the identity matrix
+x1=rand(100,100)
+[R1 U1]=house_qr(x1)
+I1=eye(size(U1));
+Q1=house_apply(U1,I1);
+Step1=Q1'*Q1; % Equals I the identity matrix
+Step2=Q1*Q1'; % Equals I the identity matrix
 % Since Q1'*Q1=I and Q1*Q1'=I, Q is orthogonal. Definition for Orthogonal
 % matrices was acquired from Google Gemini
-Step4=R1 % R1 is and upper triangular matrix
+Step4=R1; % R1 is and upper triangular matrix
 Step5=Q1*R1 % A equals Q1*R1 so the code is correct
