91b9d55b23cd8d08da6e9a8ee5a95398349a3e18
ajpx01 <ajpoon321@gmail.com>
2026-02-18 16:36:31 -0500

    Guided Lab #2 Code

--- runhouseholder_reflect.m
+++ runhouseholder_reflect.m
@@ -1,10 +1,12 @@
 %% Exercise 3
+%b)
 x=[1 1 1;1 1 0;1 0 0]; % Matrix for QR decomposition by Householder Reflections
 [R,U]=house_qr(x) % Compute decomposition
 I=eye(size(U)); % Set the identity matrix equal to the size of U
 Q=house_apply(U,I) % Solve for value Q from U and I
 QR=Q*R % Show that QR=Q*R and that the decomposition worked
 
+%c)
 x1=rand(100,100)
 [R1 U1]=house_qr(x1)
 I1=eye(size(U1));
